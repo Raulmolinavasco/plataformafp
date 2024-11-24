a5230289d840b5c32615cd2b7900863cbc5cf77f
Raul Molina <extraescolares.vascodelazarza@gmail.com>
2024-11-24 18:59:53 +0100

    segundo commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+       $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        $user2 = User::factory()->create([
+            'name' => 'Raul',
+            'email' => 'adminrmolina@educa.jcyl.e@gmail.com',
+        ]);
+        $role = Role::create(['name' => 'Admin']);
+        $user1->assignRole($role);
+        $user2->assignRole($role);
+    }
+}
